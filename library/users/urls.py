6d58eeba581d36d6d8929f513409497ee83bdd7e
shahrzad <shahrzad123azari@gmail.com>
2024-09-18 23:32:53 +0330

    change LoginView to TokenObtainPairView

--- library/users/urls.py
+++ library/users/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import LoginView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
